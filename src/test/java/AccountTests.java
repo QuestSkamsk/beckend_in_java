2e601ac50357b63ef917226a37dbac42bba4ddd2
maksimvaulin <quest.skamsk@mail.com>
2022-01-26 19:46:28 +0500

    Initial commit

---
+++ src/test/java/AccountTests.java
@@ -0,0 +1,144 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class AccountTests extends BaseTest {
+
+    @Test
+    void getAccountInfoTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountAccountWithLoggingTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getGalleryTagsTest() {
+        given()
+                .headers("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/tags")
+                .prettyPeek()
+                .then()
+                .extract()
+                .response()
+                .jsonPath()
+                .getString("data.tags.name");
+    }
+
+    @Test
+    void postFollowTagTest() {
+        given()
+                .headers("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .post("https://api.imgur.com/3/account/me/follow/tag/{tagName}", "disney")
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getAccountInfoWithAssertionsInGivenTest() {
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("data.url", equalTo(username))
+                .body("success", equalTo(true))
+                .body("status", equalTo(200))
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+
+    }
+
+    @Test
+    void putChangeAccountSettingsTest() {
+        given()
+                .headers("Authorization", token)
+                .multiPart("public_images", true)
+                .multiPart("messaging_enabled", false)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .put("https://api.imgur.com/3/account/{username}/settings", username)
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .contentType("application/json");
+    }
+
+    @Test
+    void getAccountInfoWithAssertionsAfterTest() {
+        Response response = given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/{username}", username)
+                .prettyPeek();
+        assertThat(response.jsonPath().get("data.url"), equalTo(username));
+        assertThat(response.statusCode(), equalTo(200));
+    }
+
+    @Test
+    void getAccountImagesTest() {
+        imageHash=given()
+                .headers("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .get("https://api.imgur.com/3/account/me/images")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .jsonPath()
+                .getString("data.id");
+
+    }
+
+}
---
+++ src/test/java/ImageTests.java
@@ -0,0 +1,116 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+
+public class ImageTests extends BaseTest {
+
+    private final String PATH_TO_IMAGE = "src/test/resources/3WzKURV.jpg";
+    static String encodedFile;
+    String addedImageId;
+
+    @BeforeEach
+    void beforeTest(){
+        byte[] byteArray = getFileContent();
+        encodedFile = Base64.getEncoder().encodeToString(byteArray);
+    }
+
+    @Test
+    void postAddImageTest(){
+        addedImageId = given()
+                .header("Authorization", token)
+                .multiPart("image", encodedFile)
+                .formParam("title", "act")
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .body("success", is(true))
+                .body("data.id", is(notNullValue()))
+                .when()
+                .post("https://api.imgur.com/3/image")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .jsonPath()
+                .getString("data.deletehash");
+    }
+
+    @Test
+    void postAddImageFaviritesTest(){
+        addedImageId = given()
+                .header("Authorization", token)
+                .multiPart("image", encodedFile)
+                .formParam("title", "act")
+                .log()
+                .method()
+                .log()
+                .uri()
+                .expect()
+                .body("success", is(true))
+                .body("data.id", is(notNullValue()))
+                .when()
+                .post("https://api.imgur.com/3/image")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .jsonPath()
+                .getString("data.id");
+        addFavirites();
+    }
+
+    void addFavirites(){
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .post("https://api.imgur.com/3/image/{imageHash}/favorite", addedImageId)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void postUnfavoritedImage(){
+        postAddImageFaviritesTest();
+        addFavirites();
+    }
+
+    private byte[] getFileContent(){
+        byte[] byteArray = new byte[0];
+        try {
+            byteArray = FileUtils.readFileToByteArray(new File(PATH_TO_IMAGE));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return byteArray;
+    }
+
+    @AfterEach
+    void takeDown(){
+        given()
+                .header("Authorization", token)
+                .log()
+                .method()
+                .log()
+                .uri()
+                .when()
+                .delete("https://api.imgur.com/3/account/{username}/image/{deleteHash}", username, addedImageId)
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+}
